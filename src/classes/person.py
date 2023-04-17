db040dc7ca4a9c8d7dca34b9860bd97d2845c568
Camilo Cañaveral <camilo.lam93@gmail.com>
2023-04-17 08:11:13 -0500

    apdate

--- src/classes/person.py
+++ src/classes/person.py
@@ -1,15 +1,16 @@
 class person: 
     id_person = 0 
-    person_type = ''
     name = '' # full name
     document = '' # identification number
-    entrance = '' # date
-    exit_ = '' # date
-    early_exit = '' # three options
+    id_type = 0
+    id_area = 0
+    state = '' # into the company or out the company
+    
 
-    def  __init__(self,id_person, name, document, person_type ): 
+    def  __init__(self,id_person, name, document, id_type, id_area): 
         self.id_person = id_person
         self.name = name
         self.document = document
-        self.person_type = person_type
+        self.id_type = id_type
+        self.id_area = id_area
 
--- src/main.py
+++ src/main.py
@@ -17,11 +17,31 @@ class income_control:
     guests = []
     suppliers = []
     people = 0
-
-
-
+    db = mysql.mysqlDb()
+    
+    def __init__(self):
+        self.types = self.db.query_select_all("SELECT * FROM person_type ")
+        #logging.debug(self.types[0])
+        registros = self.db.query_select_all('select * from people_list')
+        for p in registros: 
+            per = person.person(p[0],p[1],p[2],p[3],p[4])
+            if p[3]==1:
+                self.employs.append(per)
+            elif p[3] == 2:
+                self.guests.append(per)
+            elif p[3] == 3:
+                self.suppliers.append(per)
+        
+        logging.debug(self.types)
+        logging.debug(self.employs)
+        logging.debug(self.guests)
+        logging.debug(self.suppliers)
+    
+    def all_people(self):
+        return (len(self.employs)+len(self.guests)+len(self.employs))
+        
 
 if __name__=='__main__':
-    db1=mysql.mysqlDb()
-    datos=db1.query_select_all("SELECT * FROM empleados")
-    logging.info(datos)
+    company = income_control()
+    #datos=company.db.query_select_all("SELECT * FROM people_list")
+    #logging.info(datos)
